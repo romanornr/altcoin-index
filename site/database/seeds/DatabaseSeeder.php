a2c7730411a4d6b224df5ea6cdb31988a100ef33
romanornr <romanornr@gmail.com>
2017-02-22 17:21:11 +0100

    market seeder

--- site/database/seeds/DatabaseSeeder.php
+++ site/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use MarketTableSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,63 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+        $this->call('MarketTableSeeder');
     }
 }
+
+class MarketTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('markets')->insert([
+            'name' => 'ethereum',
+            'symbol' => 'eth',
+        ]);
+
+        DB::table('markets')->insert([
+            'name' => 'litecoin',
+            'symbol' => 'ltc',
+        ]);
+
+        DB::table('markets')->insert([
+            'name' => 'monero',
+            'symbol' => 'xmr',
+        ]);
+
+        DB::table('markets')->insert([
+            'name' => 'dash',
+            'symbol' => 'dash',
+        ]);
+
+        DB::table('markets')->insert([
+            'name' => 'ethereum-classic',
+            'symbol' => 'etc',
+        ]);
+
+        DB::table('markets')->insert([
+            'name' => 'maidsafecoin',
+            'symbol' => 'maid',
+        ]);
+
+        DB::table('markets')->insert([
+            'name' => 'nem',
+            'symbol' => 'nem',
+        ]);
+
+        DB::table('markets')->insert([
+            'name' => 'factom',
+            'symbol' => 'fct',
+        ]);
+
+        DB::table('markets')->insert([
+            'name' => 'zcash',
+            'symbol' => 'zec',
+        ]);
+    }
+}
+
